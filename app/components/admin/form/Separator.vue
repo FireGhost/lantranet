d4f504cd5a4c30f19fbd8a7dc9ba902d32ec0aa8
FireGhost <amermod@protonmail.com>
2025-10-29 23:49:44 +0100

    Started reworking admin form

---
+++ app/components/admin/form/Separator.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  title: string,
+}>();
+</script>
+
+<template>
+  <div class="text-3xl font-bold mt-4">
+    {{ title }}
+  </div>
+  <USeparator class="mt-2 mb-2" />
+  <div class="flex pt-4 gap-4 flex-wrap">
+    <slot />
+  </div>
+</template>
--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -9,35 +9,45 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="flex pt-4 gap-4 flex-wrap">
-    <UCard class="w-fit">
-      <template #header>Days of lan</template>
-      <AdminFormDaysOfLan />
-    </UCard>
+  <div>
+    <AdminFormSeparator title="Lan administration">
+      <UCard class="w-fit">
+        <template #header>Days of lan</template>
+        <AdminFormDaysOfLan />
+      </UCard>
 
-    <UCard class="w-fit">
-      <template #header>All animations</template>
-      <AdminFormAllAnimationsLinks />
-    </UCard>
+      <UCard class="w-fit">
+        <template #header>All animations</template>
+        <AdminFormAllAnimationsLinks />
+      </UCard>
+    </AdminFormSeparator>
 
-    <UCard class="w-fit">
-      <template #header>Admin users</template>
-      <AdminFormSwitchAdminUsers />
-    </UCard>
+    <AdminFormSeparator title="Buvette administration">
+      <UCard class="w-fit">
+        <template #header>Menu Categories</template>
+        <AdminFormMenuCategories />
+      </UCard>
 
-    <UCard class="w-fit">
-      <template #header>Menu Categories</template>
-      <AdminFormMenuCategories />
-    </UCard>
+      <UCard class="w-fit">
+        <template #header>Menu Items Available</template>
+        <AdminFormMenuItems />
+      </UCard>
 
-    <UCard class="w-fit">
-      <template #header>Menu Items Available</template>
-      <AdminFormMenuItems />
-    </UCard>
+      <UCard class="w-fit">
+        <template #header>Order status</template>
+        <AdminFormOrderStatus />
+      </UCard>
+    </AdminFormSeparator>
 
-    <UCard class="w-fit">
-      <template #header>Order status</template>
-      <AdminFormOrderStatus />
-    </UCard>
+    <AdminFormSeparator title="Users administration">
+      <UCard class="w-fit">
+        <template #header>Admin users</template>
+        <AdminFormSwitchAdminUsers />
+      </UCard>
+    </AdminFormSeparator>
+
+    <AdminFormSeparator title="Texts administration">
+      <span>Nothing to see here, for now...</span>
+    </AdminFormSeparator>
   </div>
 </template>
